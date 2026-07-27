120d2ae8a6acc318bbf0e68151b18842597ba4d9
md-4n4s <m.d.4n4s@gmail.com>
2026-07-27 18:10:03 +0500

    Created login function to login GitHub.

--- main.py
+++ main.py
@@ -2,19 +2,41 @@ from playwright.sync_api import sync_playwright
 import time
 from dotenv import load_dotenv
 import os
+import random
 
 load_dotenv()
 
-username = os.getenv("GITHUB_GMAIL_USERNAME")
+username = os.getenv("GITHUB_USERNAME")
 password = os.getenv("GITHUB_PASSWORD")
 
+
 def create_browser():
-    return p.chromium.launch(headless=False)
+    return p.chromium.launch(
+        headless=False
+    )
 
 def create_page(browser):
     return  browser.new_page()
 
+def login(page):
 
+    time.sleep(random.randint(1,3))
+
+    try:
+        page.fill("#login_field", username)
+
+        page.fill("#password", password)
+
+        page.locator("input[name=\"commit\"]").click()
+
+        time.sleep(random.randint(1, 3))
+
+        print(page.title())
+
+        time.sleep(10)
+
+    except:
+        print("Login failed")
 
 with sync_playwright() as p:
 
@@ -23,3 +45,4 @@ with sync_playwright() as p:
 
     page.goto("https://github.com/login")
 
+    login(page)
